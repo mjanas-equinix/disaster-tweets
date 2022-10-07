05b95c7a3e471a8dd61d571e9aedcc9cab9094dd
Marta Janas <marta.janas92@gmail.com>
2022-10-07 13:43:59 +0200

    added table with texts history

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template, request
 import tensorflow as tf
-
 from src.models.preprocess_data import TextCleanTransformer
+
 
 app = Flask(__name__)
 text_cleaner = TextCleanTransformer()
@@ -14,24 +12,35 @@ prediction_mapping = {
     0: "There's nothing to be worried about"
 }
 
-text_sent = []
+texts_sent = []
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def main():
-    return render_template('main.html')
+    text = None
+    prediction_text = None
+    # Checking request type
+    if request.method == "POST":
+        # text processing
+        text = request.form.get('tweet')
+        clean_text = text_cleaner.preprocess_sentence(text)
+        prediction = round(model.predict([clean_text])[0][0])
+        prediction_text = prediction_mapping.get(prediction)
 
+        texts_sent.append({
+            'text': text,
+            'prediction': prediction
+        })
 
-@app.route("/", methods=["POST"])
-def response():
-    # print(request.form.to_dict())
-    text = request.form.get('tweet')
-    clean_text = text_cleaner.preprocess_sentence(text)
-    prediction = model.predict([clean_text])[0][0]
-    prediction_text = prediction_mapping.get(round(prediction))
     return render_template('main.html',
                            text=text,
-                           prediction_text=prediction_text)
+                           prediction_text=prediction_text,
+                           texts_sent=texts_sent)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'page not found error', 404
 
 
 if __name__ == "__main__":
